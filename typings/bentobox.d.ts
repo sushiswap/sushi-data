be26588f049551235f656984e1971adb363b0d84
0xJiro <jirosushi@protonmail.com>
2021-04-13 16:36:02 -0700

    Added kashiStakedInfo query

--- typings/bentobox.d.ts
+++ typings/bentobox.d.ts
@@ -4,7 +4,19 @@ type Clone = {
   timestamp: number;
 }
 
+type Pair = {
+  id?: string;
+  name?: string;
+  symbol?: string;
+  asset?: string;
+  collateral?: string;
+  totalAssetBase?: number;
+  totalAssetElastic?: number;
+}
+
 export function clones({ masterAddress, chainId }?: {
   masterAddress?: string;
   chainId?: number;
 }): Promise<Clone>;
+
+export function kashiStakedInfo(): Promise<Pair>;
